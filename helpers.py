a448bf0d8570ec4a97b7fe5d37797e0f5a9ce341
emma <eqbrown@isf.edu.hk>
2025-08-13 12:14:36 +0800

    make functions more clear and fix sql commands

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 # this file has helper functions 
-# each function connects to the database
-# and runs sql commands
+# each function connects to the database and runs sql commands
 
 import sqlite3
 
@@ -19,29 +18,31 @@ def get_all_riddles():
 
     return all_riddles
 
-def increment_row_value(column, id):
+def increment_riddle_column(column, id):
     conn = sqlite3.connect('database.db') # connect to database
     conn.row_factory = sqlite3.Row  #converts row to dictionary object
 
     conn.execute(
-            f"""
-            UPDATE songs 
-            SET {column} = {column} + 1 
-            WHERE id = {id}"""
-        )
-    
+        f"""UPDATE riddles 
+        SET {column} = {column} + 1 
+        WHERE id = ?""",(id,)
+    )
+
     conn.commit()
     conn.close()
 
 
 if __name__=="__main__":
-    # -- testing helper SQL functions
+    # -- run python helpers.py to test your helper functions
+    # use comments to test section by section 
 
+    # gets all Riddles from db and prints each ID and question
     all_riddles = get_all_riddles()
     for riddle in all_riddles:
         print(riddle['id'], riddle['question'])
 
+    # increments the total_guesses for Riddle #3 (experiment with changing the number)
+    increment_row_value('total_guesses', 3)
 
-    increment_row_value('total_guesses', id)
 
     
