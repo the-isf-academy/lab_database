cb529701b38e2b538acb833d78804b436df695a2
eqbrown_teacher <nobody@makingwithcode.org>
2025-08-28 10:08:09 +0800

    add difficulty based on random total guesses and correct guesses

--- init_db.py
+++ init_db.py
@@ -1,5 +1,4 @@
-# this function creates the database file 
-# and inserts 100 riddles
+# this file creates the database file and populates it with 100 riddles
 
 import sqlite3
 import json
@@ -8,30 +7,39 @@ import random
 # connect to database
 connection = sqlite3.connect('database.db')
 
-# open songs.sql 
+# open riddles.sql 
 with open('riddles.sql') as file:
     connection.executescript(file.read())
 
 # create connection to database
 conn = connection.cursor()
 
-# create new questions in the database
 # Open and read the JSON file
 with open('riddles.json', 'r') as file:
     riddles = json.load(file)
     
+# create new riddles in the database
 for riddle in riddles:
     total_guesses = random.randint(10,50)
     correct_guesses = random.randint(0,total_guesses)
+    percentage_correct = correct_guesses/total_guesses
+    if percentage_correct < 0.3:
+        difficulty = 'hard'
+    elif 0.3 < percentage_correct < 0.6:
+        difficulty = 'medium'
+    else:
+        difficulty = 'easy'
+
+
     conn.execute(
         """
-        INSERT INTO riddles (question, answer, total_guesses, correct_guesses)
-        VALUES (?, ?, ?, ?)
+        INSERT INTO riddles (question, answer, total_guesses, correct_guesses, difficulty)
+        VALUES (?, ?, ?, ?, ?)
         """,
-        (riddle['question'], riddle['answer'], total_guesses, correct_guesses)
+        (riddle['question'], riddle['answer'], total_guesses, correct_guesses, difficulty)
     )
 
-# save new questions to database
+# save new riddles to database
 connection.commit()
 
 # close database
